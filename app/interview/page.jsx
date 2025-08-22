7dbd0af932d587f01fb6bdb63e5d6ce7269e9413
aryan-developer1 <aryandeveloper41@gmail.com>
2025-08-22 14:43:23 +0530

    correct logic of ineterview page

--- app/interview/page.jsx
+++ app/interview/page.jsx
@@ -1,7 +1,14 @@
 
+import { checkIfUserIsOnboarded } from "@/actions/user";
 import MockInterview from "./components/MockInterview"
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+   const isUserOnboarded = await checkIfUserIsOnboarded();
+    if (!isUserOnboarded) {
+      
+      return redirect("/onboarding");
+    }
   return (
     <div>
       <MockInterview/>
